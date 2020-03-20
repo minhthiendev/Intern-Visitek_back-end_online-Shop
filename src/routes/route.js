60f0acee6cf209e79594e230b05f592b8dc78454
soaicccca <minhthienpham0611@gmail.com>
2020-03-20 19:42:03 +0700

    debug api cart, add middle check admin

--- src/routes/route.js
+++ src/routes/route.js
@@ -6,12 +6,12 @@ const products_ctr = require('../controller/products');
 const JWT = require('../middlewares/Jwt');
 const upload = require('../middlewares/upload');
 const CheckAdmin = require('../middlewares/isAdmin');
-//// còn phân quyền up ,load anh, voi sủa cái cart
+
 router.route('/signUp').post(user_ctr.sendMail); //ok
 router.get(`/confirm/${user_ctr.mailToken}`, user_ctr.signUp_success);//oke
 router.route('/signin').post(user_ctr.signIn);//oke
 router.post('/changePassword', JWT.verifiToken, user_ctr.changePassword);
-router.post('/addProductToCart/:id', cart_ctr.AddtoCart);//tạm oke nhưng chưa lấy được giá của 1 sản phẩm
+router.post('/addProductToCart/:id', cart_ctr.AddtoCart);//oke
 router.get('/showCart', cart_ctr.showCart);// oke
 router.get('/saveCart', JWT.verifiToken, cart_ctr.saveCart);// oke
 
